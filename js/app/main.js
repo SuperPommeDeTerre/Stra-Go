bfe85901322d0dd2d8c6ed82ce763e3dc1d66adf
jlanglade <jlanglade@pixbuf.net>
2013-10-17 17:04:13 +0200

    Change maps to Locastan ones in png format. Handle change of map select.

--- js/app/main.js
+++ js/app/main.js
@@ -2,21 +2,26 @@
 var gGames = null,
     gModes = null,
     gMaps = null,
-    gCurrentConf = null,
+    gCurrentConf = {},
     gI18n = null;
 
 define(["jquery", "jquery-ui"], function($) {
     $(function() {
+        /*
         $.support.cors = true;
         $.ajaxPrefilter("json jsonp script", function(options) {
             options.crossDomain = true;
         });
+        */
+        $("#menu a").click(function(e) {
+            e.preventDefault();
+        });
         // Load global configuration
-        $.getJSON("./config/config.json&callback=?", {}, function(data) {
+        $.getJSON("./config/config.json", {}, function(data) {
             gGames = data.games;
 
             // Get i18n strings
-            $.getJSON("./i18n/" + data.lang.default + "/i18n.json&callback=?", {}, function(data) {
+            $.getJSON("./i18n/" + data.lang.default + "/i18n.json", {}, function(data) {
                 gI18n = data;
                 // Add the games to the corresponding menu
                 var myGames = "",
@@ -30,7 +35,8 @@ define(["jquery", "jquery-ui"], function($) {
                     e.stopImmediatePropagation();
                     e.preventDefault();
                     var myGameToken = $(this).val();
-                    $.getJSON("./config/" + myGameToken + "/maps.json&callback=?", {}, function(data) {
+                    gCurrentConf.game = myGameToken;
+                    $.getJSON("./config/" + myGameToken + "/maps.json", {}, function(data) {
                         gMaps = data;
                         var myMapToken = null,
                             myMapObj = null,
@@ -39,15 +45,30 @@ define(["jquery", "jquery-ui"], function($) {
                             myMapObj = gMaps[myMapToken];
                             myMaps += "<option value=\"" + myMapToken + "\">" + gI18n.games[myGameToken].maps[myMapToken] + "</option>";
                         }
-                        $("#selMap").append(myMaps).change(function(e) {
-                            e.stopImmediatePropagation();
-                            e.preventDefault();
-                            $("#mapContainer").html("<img src=\"./res/" + myGameToken + "/maps/" + gMaps[$(this).val()].file + "\">");
-                        }).change();
+                        $("#selMap").html(myMaps);
                     }).fail(function() {
                         console.log("Error while getting ./config/" + myGameToken + "/maps.json");
                     });
                 }).change();
+                $("#selMap").change(function(e) {
+                    e.stopImmediatePropagation();
+                    e.preventDefault();
+                    var myGameToken = $("#selGame").val(),
+                        myMapToken = $(this).val(),
+                        myMapObj = gMaps[myMapToken],
+                        myMapModes = "";
+                    gCurrentConf.map = myMapToken;
+                    $("#mapContainer").html("<img src=\"./res/" + myGameToken + "/maps/" + myMapObj.file + "\">");
+                    for (var myMode in myMapObj.modes) {
+                        myMapModes += "<option value=\"" + myMapObj.modes[myMode] + "\">" + gI18n.games[myGameToken].modes[myMapObj.modes[myMode]] + "</option>";
+                    }
+                    $("#selMode").html(myMapModes);
+                });
+                $("#selMode").change(function(e) {
+                    e.stopImmediatePropagation();
+                    e.preventDefault();
+                    gCurrentConf.mode = $(this).val();
+                });
                 updateComponents();
             }).fail(function() {
                 console.log("Error while getting ./i18n/" + data.lang.default + "/i18n.json");
