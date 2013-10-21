95eb364fb01cc536e15b6ddf355432bb3f42a5d9
jlanglade <jlanglade@pixbuf.net>
2013-10-21 16:06:55 +0200

    Now, we handle drawing of grid and flags overlays.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 require.config({
-    "baseUrl": 'js/lib',
+    "baseUrl": "js/lib",
     "paths": {
       "app": "../app"
     },
     "shim": {
         "jquery-ui": ["jquery"],
+        "jquery-svg": ["jquery"],
+        "jquery-ui-contextmenu": ["jquery-ui"],
+        "jquery-svgdom": ["jquery-svg"],
+        "jquery-svgfilter": ["jquery-svg"],
+        "jquery-svggraph": ["jquery-svg"],
+        "jquery-svgplot": ["jquery-svg"]
     }
 });
 
+require(["jquery-svg", "jquery-svgdom", "jquery-svggraph", "jquery-svgplot", "jquery-svgfilter", "app/main"]);
 // Load the main app module to start the app
-require(["app/main"]);
--- js/app/main.js
+++ js/app/main.js
@@ -3,7 +3,8 @@ var gGames = null,
     gModes = null,
     gMaps = null,
     gCurrentConf = {},
-    gI18n = null;
+    gI18n = null,
+    gDECAL_GRID = 20;
 
 define(["jquery", "jquery-ui"], function($) {
     $(function() {
@@ -45,7 +46,7 @@ define(["jquery", "jquery-ui"], function($) {
                             myMapObj = gMaps[myMapToken];
                             myMaps += "<option value=\"" + myMapToken + "\">" + gI18n.games[myGameToken].maps[myMapToken] + "</option>";
                         }
-                        $("#selMap").html(myMaps);
+                        $("#selMap").html(myMaps).change();
                     }).fail(function() {
                         console.log("Error while getting ./config/" + myGameToken + "/maps.json");
                     });
@@ -58,16 +59,103 @@ define(["jquery", "jquery-ui"], function($) {
                         myMapObj = gMaps[myMapToken],
                         myMapModes = "";
                     gCurrentConf.map = myMapToken;
-                    $("#mapContainer").html("<img src=\"./res/" + myGameToken + "/maps/" + myMapObj.file + "\">");
+
+                    // Set the map image and resize it to its real size (1px = 1m)
+                    $("#mapContainer").css("background-image", "url('./res/" + myGameToken + "/maps/" + myMapObj.file + "')")
+                        .css("background-position", "20px 20px")
+                        .css("background-size", myMapObj.size.x + "px " + myMapObj.size.y + "px")
+                        .css("background-repeat", "no-repeat")
+                        .width(myMapObj.size.x + gDECAL_GRID + "px")
+                        .height(myMapObj.size.y + gDECAL_GRID + "px");
                     for (var myMode in myMapObj.modes) {
-                        myMapModes += "<option value=\"" + myMapObj.modes[myMode] + "\">" + gI18n.games[myGameToken].modes[myMapObj.modes[myMode]] + "</option>";
+                        myMapModes += "<option value=\"" + myMode + "\">" + gI18n.games[myGameToken].modes[myMode] + "</option>";
                     }
-                    $("#selMode").html(myMapModes);
+                    $("#selMode").html(myMapModes).change();
+                    $("#gridOverlay").remove();
+                    $("#chkGrid").change();
+                    $("#windRoseOverlay").remove();
+                    $("#chkDirections").change();
+                    $("#mapDesc .mapName").text(gI18n.games[myGameToken].maps[myMapToken]);
+                    $("#mapDesc .mapMetrics").text(myMapObj.size.x + "m x " + myMapObj.size.y + "m");
+                    $("#mapDesc .mapSquareLength").text("(" + (myMapObj.size.x / 10) + "m)");
                 });
                 $("#selMode").change(function(e) {
                     e.stopImmediatePropagation();
                     e.preventDefault();
                     gCurrentConf.mode = $(this).val();
+                    $("#basesOverlay").remove();
+                    $("#chkBases").change();
+                });
+                $("#chkGrid").change(function(e) {
+                    var myCanvas = $("#mapContainer"),
+                        i = 0;
+                        j = 0;
+                    if ($(this).is(":checked")) {
+                        if ($("#gridOverlay").length === 0) {
+                            myCanvas = myCanvas.svg().svg("get"); 
+                            var g = myCanvas.group(null, "gridOverlay", {});
+                            for (i=0; i<gMaps[gCurrentConf.map].size.x; i+=gMaps[gCurrentConf.map].size.x/10) {
+                                myCanvas.text(g, i + gDECAL_GRID + gMaps[gCurrentConf.map].size.x/20, 16, String.fromCharCode(65 + j));
+                                myCanvas.text(g, 8, i + gDECAL_GRID + gMaps[gCurrentConf.map].size.x/20, String.fromCharCode(48 + j));
+                                j++;
+                                // Don't draw the first lines as they are in the border
+                                if (i === 0) {
+                                    continue;
+                                }
+                                myCanvas.line(g, i + gDECAL_GRID, gDECAL_GRID, i + gDECAL_GRID, gMaps[gCurrentConf.map].size.y + gDECAL_GRID, {"stroke": "#FFF", "stroke-width": 0.5, "stroke-dasharray": "1,1"});
+                                myCanvas.line(g, i + gDECAL_GRID, 0, i + gDECAL_GRID, gDECAL_GRID, {"stroke": "#000", "stroke-width": 0.5, "stroke-dasharray": "1,1"});
+                                myCanvas.line(g, gDECAL_GRID, i + gDECAL_GRID, gMaps[gCurrentConf.map].size.y + gDECAL_GRID, i + gDECAL_GRID, {"stroke": "#FFF", "stroke-width": 0.5, "stroke-dasharray": "1,1"});
+                                myCanvas.line(g, 0, i + gDECAL_GRID, gDECAL_GRID, i + gDECAL_GRID, {"stroke": "#000", "stroke-width": 0.5, "stroke-dasharray": "1,1"});
+                            }
+                        } else {
+                            $("#gridOverlay").show();
+                        }
+                    } else {
+                        $("#gridOverlay").hide();
+                    }
+                });
+                $("#chkDirections").change(function(e) {
+                    if ($(this).is(":checked")) {
+                        if ($("#windRoseOverlay").length === 0) {
+                            var myCanvas = $("#mapContainer").svg().svg("get"),
+                                g = myCanvas.group(null, "windRoseOverlay", {});
+                                myCanvas.image(g, gMaps[gCurrentConf.map].size.x - 100, 15, 100, 100, "./res/images/windrose.png", {});
+                        } else {
+                            $("#windRoseOverlay").show();
+                        }
+                    } else {
+                        $("#windRoseOverlay").hide();
+                    }
+                });
+                $("#chkBases").change(function(e) {
+                    if ($(this).is(":checked")) {
+                        if ($("#basesOverlay").length === 0) {
+                            var myCanvas = $("#mapContainer").svg().svg("get"),
+                                g = myCanvas.group(null, "basesOverlay", {}),
+                                myMapMode = gMaps[gCurrentConf.map].modes[gCurrentConf.mode],
+                                countDrops = 0;
+                            for (var myMapTeam in myMapMode) {
+                                countDrops = 0;
+                                for (var i in myMapMode[myMapTeam]) {
+                                    if (myMapMode[myMapTeam][i].type === "base") {
+                                        // It's the main bases, they are round
+                                        myCanvas.circle(g, myMapMode[myMapTeam][i].x + gDECAL_GRID, myMapMode[myMapTeam][i].y + gDECAL_GRID, 50, { "class": myMapTeam });
+                                    } else if (myMapMode[myMapTeam][i].type === "drop") {
+                                        // It's the drop points, they are square
+                                        myCanvas.polygon(g, [[myMapMode[myMapTeam][i].x - 40, myMapMode[myMapTeam][i].y],
+                                                             [myMapMode[myMapTeam][i].x, myMapMode[myMapTeam][i].y - 40],
+                                                             [myMapMode[myMapTeam][i].x + 40, myMapMode[myMapTeam][i].y],
+                                                             [myMapMode[myMapTeam][i].x, myMapMode[myMapTeam][i].y + 40]], { "class": myMapTeam });
+                                        myCanvas.text(g, myMapMode[myMapTeam][i].x, myMapMode[myMapTeam][i].y + 5, (countDrops++ + 1) + "", { "class": myMapTeam });
+                                    }
+                                }
+                            }
+                        } else {
+                            $("#basesOverlay").show();
+                        }
+                    } else {
+                        $("#basesOverlay").hide();
+                    }
                 });
                 updateComponents();
             }).fail(function() {
