e7b1d1102f207cb79cdaed782496722f52d674ed
jlanglade <jlanglade@pixbuf.net>
2013-11-27 11:21:24 +0100

    On peut désormais placer des éléments et les déplacer sur la carte.

--- js/app/main.js
+++ js/app/main.js
@@ -2,6 +2,7 @@
 var gGames = null,
     gModes = null,
     gMaps = null,
+    gElements = null,
     gCurrentConf = {},
     gI18n = null;
 
@@ -18,18 +19,63 @@ var gDECAL_GRID = 20,
 /**
  * Main function
  */
-define(["jquery", "jquery-ui"], function($) {
+define(["jquery", "jquery-ui", "jquery-svg"], function($) {
     $(function() {
         /*
         $.support.cors = true;
         $.ajaxPrefilter("json jsonp script", function(options) {
             options.crossDomain = true;
         });
+        $.event.props.push("dataTransfer");
         */
         $("#menu a").click(function(e) {
             e.preventDefault();
         });
         // Load global configuration
+		var myDraggedElement = null;
+        var myCanvasContainer = $("#mapContainer");
+        myCanvasContainer.on("click", function(e) {
+            e.stopImmediatePropagation();
+            e.preventDefault();
+			if (myDraggedElement !== null) {
+				return;
+			}
+            var selectedItem = $("#menuEdit .selected");
+            if (selectedItem.length > 0) {
+                if (selectedItem.is("a")) {
+                    var myItemProps = selectedItem.attr("href").split(/\//g),
+                        i = 0;
+                    if (myItemProps[2] === "element") {
+                        var elementType = myItemProps[3],
+                            elementTeam = myItemProps[4],
+                            myElem = gElements[elementType]["team" + elementTeam];
+                        var myCanvas = myCanvasContainer.svg().svg("get"),
+                            g = myCanvasContainer.find("#elementsOverlay").svg(),
+                            myImage = myCanvas.image(g, e.pageX - myCanvasContainer[0].offsetLeft - (myElem.size.x / 2), e.pageY - myCanvasContainer[0].offsetTop - (myElem.size.y / 2), myElem.size.x, myElem.size.y, "./res/" + gCurrentConf.game + "/elements/" + myElem.file, {});
+                        $(myImage).on("click", function(e) {
+							e.stopImmediatePropagation();
+							e.preventDefault();
+							if (e.which === 1) {
+								if (myDraggedElement === null) {
+									myDraggedElement = $(this);
+									myDraggedElement.addClass("moving");
+								} else {
+									myDraggedElement.removeClass("moving");
+									myDraggedElement = null;
+								}
+							}
+                        });
+                    }
+                }
+            }
+        })
+		// Handle movement of items
+		.on("mousemove", function(e) {
+			if (myDraggedElement !== null) {
+				myDraggedElement.attr("x", e.pageX - myCanvasContainer[0].offsetLeft - (myDraggedElement.attr("width") / 2));
+				myDraggedElement.attr("y", e.pageY - myCanvasContainer[0].offsetTop - (myDraggedElement.attr("height") / 2));
+			}
+        });
         $.getJSON("./config/config.json", {}, function(data) {
             gGames = data.games;
 
@@ -51,9 +97,14 @@ define(["jquery", "jquery-ui"], function($) {
                     gCurrentConf.game = myGameToken;
                     $.getJSON("./res/" + myGameToken + "/game.json", {}, function(data) {
                         gMaps = data.maps;
+                        gElements = data.elements;
                         var myMapToken = null,
                             myMapObj = null,
-                            myMaps = "";
+                            myMaps = "",
+                            myElementToken = null,
+                            myElements0 = "";
+                            myElements1 = "";
+                            myElements2 = "";
                         for (myMapToken in gMaps) {
                             myMapObj = gMaps[myMapToken];
                             myMaps += "<option value=\"" + myMapToken + "\">" + gI18n.games[myGameToken].maps[myMapToken] + "</option>";
@@ -67,6 +118,27 @@ define(["jquery", "jquery-ui"], function($) {
                             break;
                         }
                         $("#selMap").change();
+                        // Build the html for the elements
+                        for (myElementToken in gElements) {
+                            if (gElements[myElementToken].team0) {
+                                myElements0 += "<li><a href=\"edit/add/element/" + myElementToken + "/0\" class=\"element " + myElementToken + "0\" title=\"" + gI18n.games[myGameToken].elements[myElementToken] + "\"><span>" + gI18n.games[myGameToken].elements[myElementToken] + "</span></a></li>";
+                            }
+                            if (gElements[myElementToken].team1) {
+                                myElements1 += "<li><a href=\"edit/add/element/" + myElementToken + "/1\" class=\"element " + myElementToken + "1\" title=\"" + gI18n.games[myGameToken].elements[myElementToken] + "\"><span>" + gI18n.games[myGameToken].elements[myElementToken] + "</span></a></li>";
+                            }
+                            if (gElements[myElementToken].team2) {
+                                myElements2 += "<li><a href=\"edit/add/element/" + myElementToken + "/2\" class=\"element " + myElementToken + "2\" title=\"" + gI18n.games[myGameToken].elements[myElementToken] + "\"><span>" + gI18n.games[myGameToken].elements[myElementToken] + "</span></a></li>";
+                            }
+                        }
+                        // Clear old elements and add the new ones
+                        $("#elements").find("legend").siblings().remove();
+                        $("#elements").append("<section class=\"flex flex-h\"><aside class=\"flex-start\"><ul class=\"elements\">" + myElements0 + "</ul></aside><aside class=\"flex-fluid\"><ul class=\"elements\">" + myElements1 + "</ul></aside><aside class=\"flex-end\"><ul class=\"elements\">" + myElements2 + "</ul></aside></section>");
+                        $("#menuEdit").find(".element").click(function(e) {
+                            e.stopImmediatePropagation();
+                            e.preventDefault();
+                            $("#menuEdit").find(".selected").removeClass("selected")
+                            $(this).toggleClass("selected");
+                        });
                     }).fail(function() {
                         console.log("Error while getting ./res/" + myGameToken + "/game.json");
                     });
@@ -81,8 +153,8 @@ define(["jquery", "jquery-ui"], function($) {
                     gCurrentConf.map = myMapToken;
 
                     // Set the map image and resize it to its real size (1px = 1m)
-                    $("#mapContainer").css("background-image", "url('./res/" + myGameToken + "/maps/" + myMapObj.file + "')")
-                        .css("background-position", "20px 20px")
+                    myCanvasContainer.css("background-image", "url('./res/" + myGameToken + "/maps/" + myMapObj.file + "')")
+                        .css("background-position", gDECAL_GRID + "px " + gDECAL_GRID + "px")
                         .css("background-size", myMapObj.size.x + "px " + myMapObj.size.y + "px")
                         .css("background-repeat", "no-repeat")
                         .width(myMapObj.size.x + gDECAL_GRID + "px")
@@ -102,11 +174,22 @@ define(["jquery", "jquery-ui"], function($) {
                     $("#gridOverlay").remove();
                     $("#chkGrid").change();
                     $("#windRoseOverlay").remove();
+                    $("#elementsOverlay").remove();
+                    $("#linesOverlay").remove();
+                    $("#shapesOverlay").remove();
+                    $("#zonesOverlay").remove();
+                    $("#textsOverlay").remove();
                     $("#chkDirections").change();
                     $("#mapDesc .mapName").text(gI18n.games[myGameToken].maps[myMapToken]);
                     $("#mapDesc .mapMetrics").text(myMapObj.size.x + "m x " + myMapObj.size.y + "m");
                     $("#mapDesc .mapSquareLength").text("(1px = 1m)");
                     $("#txtImportExport").val(JSON.stringify(gCurrentConf));
+                    var myCanvas = myCanvasContainer.svg().svg("get");
+                    myCanvas.group(null, "elementsOverlay", {});
+                    myCanvas.group(null, "linesOverlay", {});
+                    myCanvas.group(null, "shapesOverlay", {});
+                    myCanvas.group(null, "zonesOverlay", {});
+                    myCanvas.group(null, "textsOverlay", {});
                 });
                 $("#selMode").change(function(e) {
                     e.stopImmediatePropagation();
@@ -117,19 +200,20 @@ define(["jquery", "jquery-ui"], function($) {
                     $("#txtImportExport").val(JSON.stringify(gCurrentConf));
                 });
                 $("#chkGrid").change(function(e) {
-                    var myCanvas = $("#mapContainer"),
-                        i = 0;
+                    var i = 0;
                         j = 0;
                     if ($(this).is(":checked")) {
                         if ($("#gridOverlay").length === 0) {
-                            myCanvas = myCanvas.svg().svg("get"); 
-                            var g = myCanvas.group(null, "gridOverlay", {});
+                            var myCanvas = myCanvasContainer.svg().svg("get"),
+                                g = myCanvas.group(null, "gridOverlay", {});
                             for (i=0; i<gMaps[gCurrentConf.map].size.x; i+=gMaps[gCurrentConf.map].size.x/gNB_COLS) {
+                                // Last column is 0
                                 if (j < gNB_COLS - 1) {
                                     myCanvas.text(g, i + gDECAL_GRID + gMaps[gCurrentConf.map].size.x/(gNB_COLS * 2), 16, String.fromCharCode(gCHAR_CODE_1 + j));
                                 } else {
                                     myCanvas.text(g, i + gDECAL_GRID + gMaps[gCurrentConf.map].size.x/(gNB_COLS * 2), 16, "0");
                                 }
+                                // Skip the I row
                                 if (j < 8) {
                                     myCanvas.text(g, 8, i + gDECAL_GRID + gMaps[gCurrentConf.map].size.x/(gNB_ROWS * 2), String.fromCharCode(gCHAR_CODE_A + j));
                                 } else {
@@ -155,9 +239,9 @@ define(["jquery", "jquery-ui"], function($) {
                 $("#chkDirections").change(function(e) {
                     if ($(this).is(":checked")) {
                         if ($("#windRoseOverlay").length === 0) {
-                            var myCanvas = $("#mapContainer").svg().svg("get"),
+                            var myCanvas = myCanvasContainer.svg().svg("get"),
                                 g = myCanvas.group(null, "windRoseOverlay", {});
-                                myCanvas.image(g, gMaps[gCurrentConf.map].size.x - gWIND_ROSE_SIZE, 15, gWIND_ROSE_SIZE, gWIND_ROSE_SIZE, "./res/images/windrose.png", {});
+                            myCanvas.image(g, gMaps[gCurrentConf.map].size.x - gWIND_ROSE_SIZE + gDECAL_GRID, gDECAL_GRID, gWIND_ROSE_SIZE, gWIND_ROSE_SIZE, "./res/images/windrose.png", {});
                         } else {
                             $("#windRoseOverlay").show();
                         }
@@ -168,7 +252,7 @@ define(["jquery", "jquery-ui"], function($) {
                 $("#chkBases").change(function(e) {
                     if ($(this).is(":checked")) {
                         if ($("#basesOverlay").length === 0) {
-                            var myCanvas = $("#mapContainer").svg().svg("get"),
+                            var myCanvas = myCanvasContainer.svg().svg("get"),
                                 g = myCanvas.group(null, "basesOverlay", {}),
                                 myMapMode = gMaps[gCurrentConf.map].modes[gCurrentConf.mode],
                                 countDrops = 0;
@@ -188,6 +272,16 @@ define(["jquery", "jquery-ui"], function($) {
                                     }
                                 }
                             }
+                            $("#basesOverlay").find("circle").draggable()
+                            .bind('mousedown', function(event, ui){
+                                // bring target to front
+                                $(event.target.parentElement).append(event.target);
+                            })
+                            .bind('drag', function(event, ui){
+                                // update coordinates manually, since top/left style props don't work on SVG
+                                event.target.setAttribute('x', ui.position.left);
+                                event.target.setAttribute('y', ui.position.top);
+                            });
                         } else {
                             $("#basesOverlay").show();
                         }
